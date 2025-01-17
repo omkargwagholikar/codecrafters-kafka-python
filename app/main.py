ae4dd5222686bb0d1ab47935a14dfdbbc1eeab96
omkargwagholikar <omkargwagholikar@gmail.com>
2025-01-17 13:14:16 +0530

    parsing correlation id in client messages

--- app/main.py
+++ app/main.py
@@ -6,17 +6,27 @@ def create_message(id):
 
 def handle_client(client):
     request = client.recv(1024) # reading the first 1024 bytes sent by the client, this is a placeholder value for now
-    print(f"[+] Recieved: {request}")   
-    client.sendall(create_message(7))
+    print(f"[+] Recieved: {request}")
+    
+    correlation_id = int.from_bytes(request[8:12], byteorder='big')
+    print(f"[+] Received correlation_id: {correlation_id}")
+    
+    client.sendall(create_message(correlation_id))
     client.close()
 
+#core message sizes:
+# 4 bytes for the length of the message
+# Example structure:
+# 00 00 00 23  // message_size:        35 
+# 00 12        // request_api_key:     18 INT16
+# 00 04        // request_api_version: 4 INT16
+# 6f 7f c6 61  // correlation_id:      1870644833 INT32
+# ...
+# echo -n "00000023001200046f7fc66100096b61666b612d636c69000a6b61666b612d636c6904302e3100" | xxd -r -p | nc localhost 9092 | hexdump -C
+
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     
     while True:
