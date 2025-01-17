f5d0cc8d3c6d8de9d8fd06e8644ef84d4a30992b
omkargwagholikar <omkargwagholikar@gmail.com>
2025-01-17 13:00:55 +0530

    sending correlation id's

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import socket  # noqa: F401
 
+def create_message(id):
+    id_bytes = id.to_bytes(4, byteorder="big")
+    return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
+
+def handle_client(client):
+    request = client.recv(1024) # reading the first 1024 bytes sent by the client, this is a placeholder value for now
+    print(f"[+] Recieved: {request}")   
+    client.sendall(create_message(7))
+    client.close()
 
 def main():
     # You can use print statements as follows for debugging,
@@ -9,8 +18,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
-
+    
+    while True:
+        client, addrs = server.accept()
+        handle_client(client)
 
 if __name__ == "__main__":
     main()
